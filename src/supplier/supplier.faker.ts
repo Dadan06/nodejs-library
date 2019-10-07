b0a171d2410c6b4e979410c9e6712118e615628f
= <ismael.fidimalala@dreamslab.fr>
2019-10-07 11:52:42 +0300

    Create supplier faker

---
+++ src/supplier/supplier.faker.ts
@@ -0,0 +1,17 @@
+import * as faker from 'faker';
+import { mongoDbObjectId, writeToJson } from '../shared/utils/faker.utils';
+import { Supplier } from './supplier.model';
+
+const NB_SUPPLIER = 30;
+
+const suppliers: Supplier[] = Array.from(
+    new Array(NB_SUPPLIER),
+    (): Supplier => ({
+        _id: { $oid: mongoDbObjectId() },
+        name: faker.company.companyName(),
+        address: faker.address.city(),
+        contact: faker.phone.phoneNumber()
+    })
+);
+
+writeToJson<Supplier>('suppliers.json', suppliers);
