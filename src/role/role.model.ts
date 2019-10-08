0e15d3349651d9c1c439197c8e85adb78492e5cf
= <ismael.fidimalala@dreamslab.fr>
2019-10-08 15:29:25 +0300

    [ROLE] Add roleType field #master

--- src/role/role.model.ts
+++ src/role/role.model.ts
@@ -1,9 +1,14 @@
 import { ObjectId } from 'mongodb';
 import { Privilege } from '../privilege/privilege.model';
 
+export enum RoleType {
+    ROOT = 'ROOT'
+}
+
 export interface Role {
     // tslint:disable-next-line: no-any
     _id: any;
     name: string;
     privileges: Privilege[] | ObjectId[];
+    roleType?: RoleType;
 }
--- src/role/role.schema.ts
+++ src/role/role.schema.ts
@@ -1,10 +1,15 @@
 import * as mongoose from 'mongoose';
-import { Role } from './role.model';
+import { Role, RoleType } from './role.model';
 
 export interface RoleDocument extends Role, mongoose.Document {}
 
 const schema = new mongoose.Schema({
     name: String,
+    roleType: {
+        type: String,
+        enum: Object.values(RoleType),
+        required: false
+    },
     privileges: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Privilege' }]
 });
 
