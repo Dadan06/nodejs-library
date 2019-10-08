d4917706885c3e061133a64cca8a178c8a1520eb
= <ismael.fidimalala@dreamslab.fr>
2019-10-08 14:27:16 +0300

    [PRODUCT] Add supplier #master

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,7 +1,10 @@
+import { Supplier } from '../supplier/supplier.model';
+
 export interface Product {
     // tslint:disable-next-line: no-any
     _id: any;
     name: string;
     costPrice: number;
     sellingPrice: number;
+    supplier: string | Supplier;
 }
--- src/product/product.schema.ts
+++ src/product/product.schema.ts
@@ -6,7 +6,8 @@ export interface ProductDocument extends Product, mongoose.Document {}
 const schema = new mongoose.Schema({
     name: { type: String, required: true },
     costPrice: { type: Number, required: true },
-    sellingPrice: { type: Number, required: true }
+    sellingPrice: { type: Number, required: true },
+    supplier: { type: mongoose.Schema.Types.ObjectId, ref: 'Supplier' }
 });
 
 export const productSchema = mongoose.model<ProductDocument>('Product', schema);
