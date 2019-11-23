5a8556761533e2f3bfe7b2ff7177733f05f4c698
FIDIMALALA <chiyangchiying@gmail.com>
2019-11-23 11:01:31 +0400

    [SUPPLIER] Add new fields #master

--- src/supplier/supplier.model.ts
+++ src/supplier/supplier.model.ts
@@ -2,6 +2,9 @@ export interface Supplier {
     // tslint:disable-next-line: no-any
     _id: any;
     name: string;
+    nif?: string;
+    stat?: string;
+    cin?: string;
     address: string;
     contact: string;
 }
--- src/supplier/supplier.schema.ts
+++ src/supplier/supplier.schema.ts
@@ -5,6 +5,9 @@ export interface SupplierDocument extends Supplier, mongoose.Document {}
 
 const schema = new mongoose.Schema({
     name: { type: String, required: true },
+    nif: { type: String, required: false },
+    stat: { type: String, required: false },
+    cin: { type: String, required: false },
     address: { type: String, required: true },
     contact: { type: String, required: true }
 });
